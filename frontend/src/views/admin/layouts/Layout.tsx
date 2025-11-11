20d17850b1541d2fbf6548686aaaf0049171eb23
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-11-11 19:18:04 +0100

    fix: fixed error in update logic

--- frontend/src/views/admin/layouts/Layout.tsx
+++ frontend/src/views/admin/layouts/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import MainContainer from './components/mainContainer/MainContainer';
 import Main from './components/main/Main';
 import Header from './components/header/Header';
-import Sidebar from './components/sidebar/MobileSidebar';
+// import Sidebar from './components/sidebar/MobileSidebar';
 
 
 const LayoutPage = () => {
