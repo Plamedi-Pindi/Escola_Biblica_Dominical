2cc95cc6cce06dbb8acd0212652a8829fa55bcf6
Plamedi-Pindi <plamedipindi77@gmail.com>
2026-01-21 10:45:33 +0100

    fixe: timeout of 5000ms exceeded problem

--- frontend/src/services/apis/MainAPI.tsx
+++ frontend/src/services/apis/MainAPI.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const MainAPI = axios.create({
     // baseURL: 'http://127.0.0.1:3000/',
     baseURL: 'https://escola-biblica-dominical-erp.onrender.com/',
-    timeout: 5000,
+    timeout: 10000,
 })
 
 export default MainAPI;
