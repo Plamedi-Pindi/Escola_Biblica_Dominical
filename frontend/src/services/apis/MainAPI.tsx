5da3284e5c323104242c41b93e6dc0c6fa3ce59d
Plamedi-Pindi <plamedipindi77@gmail.com>
2026-01-21 10:39:49 +0100

    fixe: vercel failure

--- frontend/src/services/apis/MainAPI.tsx
+++ frontend/src/services/apis/MainAPI.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const MainAPI = axios.create({
-    baseURL: 'http://127.0.0.1:3000/',
-    // baseURL: 'https://escola-biblica-dominical-erp.onrender.com/',
+    // baseURL: 'http://127.0.0.1:3000/',
+    baseURL: 'https://escola-biblica-dominical-erp.onrender.com/',
     timeout: 5000,
 })
 
